e84e13138cc9dad99114163398342293cbbb8e15
Clark <rclark010@gmail.com>
2019-02-07 17:44:10 -0700

    Styling touches for carousel. Alt text for carousel images.

--- js/carousel.js
+++ js/carousel.js
@@ -76,6 +76,7 @@ constructor(contentData) {
     this.carouselImage = document.createElement('img');
     this.carouselImage.classList.add('carousel-image');
     this.carouselImage.src = contentData.imgSrc;
+    this.carouselImage.alt = contentData.imgAlt;
     // ––– Captions ––– (remove or customize, as needed) ––– //
     // create caption holder
     this.carouselCaption = document.createElement('div');
@@ -102,11 +103,11 @@ constructor(contentData) {
   
   // ––– Testimonial Data ––– //
 const testimonials = [
-    {name: 'Susan B', userSince: new Date('January 15, 2018'), workoutStyle: 'Interval Training', imgSrc: './images/scott-webb-22420-unsplash.jpg', quote: 'I just LOVE how easy it is to complete my interval workouts – with two taps, you\'ve logged your interval, and you\'re onto the next one. You never lose your flow.'},
-    {name: 'Todd F', userSince: new Date('February 13, 2018'), workoutStyle: 'Body Weight', imgSrc: './images/rawpixel-1064349-unsplash.jpg', quote: 'Other workout apps don\'t care about people who use body weight to workout, like me. They make you create custom exercises for everything – even pushups. But FIT ME doesn\'t. They are ready to go with a TON of built-in exercises.'},
-    {name: 'Julian A', userSince: new Date('March 25, 2018'), workoutStyle: 'Interval Training', imgSrc: './images/trust-tru-katsande-592915-unsplash.jpg', quote: 'I workout to stay in shape – I don\'t know about the technical details about macronutrients and carb loading. FIT ME is perfect for my needs. Keeps me motivated toward improving my performance, both for this week and for the long-term. I\'ve never felt better.'},
-    {name: 'Celina M', userSince: new Date('July 3, 2017'), workoutStyle: 'Professional', imgSrc: './images/alora-griffiths-781444-unsplash.jpg', quote: 'My workouts are my job. I know I need to see results, and I don\'t want a ton of fuss to track them. So I use FIT ME to keep me in line and to analyze my performance. It couldn\'t be easier.'},
-    {name: 'Ophelia Q', userSince: new Date('December 15, 2017'), workoutStyle: 'Professional', imgSrc: './images/geert-pieters-535211-unsplash.jpg', quote: 'I\'m a professional fitness instructor, and I always encourage my clients to use FIT ME to track their progress. It comes built-in with exercises and machines to fit any of my diverse clients\' needs, so I bet it\'ll be a perfect fit for you, too.'}
+    {name: 'Susan B', userSince: new Date('January 15, 2018'), workoutStyle: 'Interval Training', imgSrc: './images/scott-webb-22420-unsplash.jpg', imgAlt: 'Lady in a gym swinging a heavy rope in each hand', quote: 'I just LOVE how easy it is to complete my interval workouts – with two taps, you\'ve logged your interval, and you\'re onto the next one. You never lose your flow.'},
+    {name: 'Todd F', userSince: new Date('February 13, 2018'), workoutStyle: 'Body Weight', imgSrc: './images/rawpixel-1064349-unsplash.jpg', imgAlt: 'A man on a beach swinging a heavy rope in each hand', quote: 'Other workout apps don\'t care about people who use body weight to workout, like me. They make you create custom exercises for everything – even pushups. But FIT ME doesn\'t. They are ready to go with a TON of built-in exercises.'},
+    {name: 'Julian A', userSince: new Date('March 25, 2018'), workoutStyle: 'Interval Training', imgSrc: './images/trust-tru-katsande-592915-unsplash.jpg', imgAlt: 'A man in a gym curling a dumbbell', quote: 'I workout to stay in shape – I don\'t know about the technical details about macronutrients and carb loading. FIT ME is perfect for my needs. Keeps me motivated toward improving my performance, both for this week and for the long-term. I\'ve never felt better.'},
+    {name: 'Celina M', userSince: new Date('July 3, 2017'), workoutStyle: 'Professional', imgSrc: './images/alora-griffiths-781444-unsplash.jpg', imgAlt: 'A woman straining to squat a barbell full of weight while a person on each side spots her.', quote: 'My workouts are my job. I know I need to see results, and I don\'t want a ton of fuss to track them. So I use FIT ME to keep me in line and to analyze my performance. It couldn\'t be easier.'},
+    {name: 'Ophelia Q', userSince: new Date('December 15, 2017'), workoutStyle: 'Professional', imgSrc: './images/geert-pieters-535211-unsplash.jpg', imgAlt: 'A woman leading a workout class', quote: 'I\'m a professional fitness instructor, and I always encourage my clients to use FIT ME to track their progress. It comes built-in with exercises and machines to fit any of my diverse clients\' needs, so I bet it\'ll be a perfect fit for you, too.'}
 ];
 
 // create carousel with testimonial data
